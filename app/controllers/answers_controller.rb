fddebb3a913ca880e5eee1c30e205ec2ff089042
Joshua Ugba <justjosh001@gmail.com>
2016-11-05 09:44:12 +0100

    Create relationship between questions and answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,9 @@
 class AnswersController < ApplicationController
+
   def create
-    @answer = Answer.create(answer_params)
-    redirect_to root_path
+    question = Question.find(params[:answer][:question_id])
+    question.answers.create(answer_params)
+    redirect_to question
   end
 
   private
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,6 @@
 class Answer < ApplicationRecord
+
+  include HasGravatar
+  belongs_to :question
+
 end
