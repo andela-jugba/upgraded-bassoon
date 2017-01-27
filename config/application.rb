b7ce6750479854456a48e6b50de68b410cc1443b
Joshua Ugba <justjosh001@gmail.com>
2017-01-27 16:04:07 +0100

    tag new release v0.1.3

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module ViewsDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.version = File.read(File.expand_path("../../VERSION", __FILE__))
   end
 end
