7b1aa99db6392eb5b4d435cf1ef01635df6a063c
Joshua Ugba <justjosh001@gmail.com>
2017-02-23 00:08:47 +0100

    Updates release script

--- release.sh
+++ release.sh
@@ -2,5 +2,7 @@
 gem install bump
 github_changelog_generator
 git add CHANGELOG.md
-bump "$1" --tag --no-bundle
+bump "$1" --no-bundle
+git commit --amend -m "Bump Version to: v$(<VERSION) and Update CHANGELOG."
+git tag "v$(<VERSION)"
 git push && git push --tags
