942714f0667f1928403baebbf4046ca2459d8ded
Joshua Ugba <justjosh001@gmail.com>
2017-04-05 16:08:24 +0100

    Removes personal copy of versionator

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gem install bump
-github_changelog_generator --since-tag v0.3.23
+github_changelog_generator --since-tag v0.3.23 --max-issues 
 git add CHANGELOG.md
 bump "$1" --no-bundle
 git commit --amend -m "Bump Version to: v$(<VERSION) and Update CHANGELOG."
